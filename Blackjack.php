8b68f8d663a18d0081ce30fab9be64da50489067
galalkoro <koro-galal@outlook.com>
2022-06-27 16:05:06 +0200

    modified

--- Blackjack.php
+++ Blackjack.php
@@ -4,5 +4,31 @@ namespace Blackjack;
 
 class Blackjack
 {
+private player $player;
+private dealer $dealer;
+private deck $deck;
 
+    /**
+     * @return player
+     */
+    public function getPlayer(): player
+    {
+        return $this->player;
+    }
+
+    /**
+     * @return dealer
+     */
+    public function getDealer(): dealer
+    {
+        return $this->dealer;
+    }
+
+    /**
+     * @return deck
+     */
+    public function getDeck(): deck
+    {
+        return $this->deck;
+    }
 }
