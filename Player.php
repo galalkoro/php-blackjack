5796d98c5358ed1faec3fd8613b3a04edc94294d
galalkoro <koro-galal@outlook.com>
2022-06-28 12:43:13 +0200

    player class modified by adding constructor and foreach loop & logic function

--- Player.php
+++ Player.php
@@ -6,10 +6,14 @@ class Player {
     private array $cards = [];
     private bool $lost = false;
 
-
     //constructor to require deck object, and then passing deck from the blackjack.
     // two cards for player that exist from the deck class.
-
+    public function __construct(Deck $deck)
+    {
+        for($i = 0; $i < 2; $i++){
+            $this->cards[] = $deck->drawCard();
+        }
+    }
 
     /**
      * @return array
@@ -27,31 +31,35 @@ class Player {
         return $this->lost;
     }
 
-
     //hit empty method
-    public function hit()
+    public function hit($deck)
     {
-
+        $this->cards[] = $deck->drawCard();
+        if ($this->getCards()>21){
+            $this->hasLost();
+        }
+        return $this->cards;
     }
 
     //surrender empty method
     public function surrender()
     {
-
+        $this->hasLost();
     }
 
     //getScore empty method
     public function getScore()
     {
-
+        $score = 0;
+        foreach ($this->cards as $card){
+            $score += $card;
+        }
+        return $score;
     }
-
 
     //hasLost empty method
     public function hasLost()
     {
         return $this->lost = true;
-
     }
-
 }
